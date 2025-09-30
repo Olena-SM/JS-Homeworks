8abbca89601764ce661e05b3c32b9bfecf790b60
Olena-SM <olenka456@gmail.com>
2025-09-30 12:11:49 +0300

    new regulyarka

--- js_homework7.js
+++ js_homework7.js
@@ -1,3 +1,3 @@
 const reg = /\b(?!\w*[aA])\w{6,}\b/g;
 const options = "Wonderful Joyful Happiness Time Task Apple";
-console.log(options.match(reg)); // ["Wonderful", "Joyful"]
+console.log(options.match(reg));
